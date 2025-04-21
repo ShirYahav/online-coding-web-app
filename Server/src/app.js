4d5a9af3fb6d1f2e19f04882a80694f3d1b54dff
ShirYahav <shiryahav07@gmail.com>
2025-04-22 00:03:25 +0300

    updated cors origin

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,9 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import cors from "cors";
-import express from 'express';
+import express from "express";
 import http from "http";
-import codeBlockController from './controller/codeblock_controller.js';
+import codeBlockController from "./controller/codeblock_controller.js";
 import dal from "./dal/dal.js";
 import setupSocket from "./logic/socket_logic.js";
 
@@ -13,12 +13,20 @@ dal.connect();
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors({ origin: '*' }));
+app.use(
+  cors({
+    origin: "https://online-coding-web-app-client-hnla.onrender.com",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use("/api" ,codeBlockController);
+app.use("/api", codeBlockController);
 
 const port = process.env.PORT || 3001;
 
 setupSocket(server);
 
-server.listen(port, () => console.log(`listening (HTTP + WebSocket) on ${port}…`));
+server.listen(port, () =>
+  console.log(`listening (HTTP + WebSocket) on ${port}…`)
+);
