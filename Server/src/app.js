45a074980302c34dfaf9d223424cdd4de09e1e98
ShirYahav <shiryahav07@gmail.com>
2025-04-22 16:25:32 +0300

    trying to fix 502 bad getaway

--- Server/src/app.js
+++ Server/src/app.js
@@ -5,22 +5,14 @@ import cors from "cors";
 import express from "express";
 import http from "http";
 import codeBlockController from "./controller/codeblock_controller.js";
-import dal from "./dal/dal.js";
 import setupSocket from "./logic/socket_logic.js";
-
-dal.connect();
+import dal from "./dal/dal.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("OK");
-});
-
-const server = http.createServer(app);
-
 const allowedOrigins = [
   "https://online-coding-web-app-client-hnla.onrender.com",
-  "http://localhost:5173"
+  "http://localhost:5173",
 ];
 
 app.use(
@@ -30,13 +22,18 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json());
+app.get("/", (_, res) => res.status(200).send("OK"));
 app.use("/api", codeBlockController);
 
-const port = process.env.PORT || 3001;
-
+const server = http.createServer(app);
 setupSocket(server);
+
+const port = process.env.PORT || 3001;
 
 server.listen(port, () =>
   console.log(`listening (HTTP + WebSocket) on ${port}…`)
 );
+
+dal.connect();
